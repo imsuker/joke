583a5ae57ff635f06d711366371b68b723d41133
imsuker <imsuker@qq.com>
2013-08-13 22:30:23 +0800

    VIPinfo

--- Classes/Controllers/VipIntroduceViewController.m
+++ Classes/Controllers/VipIntroduceViewController.m
@@ -29,7 +29,31 @@
     // Do any additional setup after loading the view from its nib.
     
     [Util adjustBackgroundImage:_buttonRegistered];
-    _viewContent.backgroundColor = [UIColor colorWithPatternImage:[Util adjustImage:[UIImage imageNamed:@"contentbox"]]];
+    
+    
+    //返回按钮
+    UIImageView *backView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+    backView.image = [UIImage imageNamed:@"back"];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backView];
+    UITapGestureRecognizer *tapBack = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapBack)];
+    [backView addGestureRecognizer:tapBack];
+    
+    //标题
+    UILabel *labelTitle = [[UILabel alloc] init];
+    labelTitle.text = @"VIP皇冠会员";
+    [labelTitle sizeToFit];
+    labelTitle.backgroundColor = [UIColor clearColor];
+    labelTitle.font = [UIFont systemFontOfSize:20.0];
+    labelTitle.textColor = JD_FONT_COLOR_fff;
+    self.navigationItem.titleView = labelTitle;
+    
+    //内容区域的圆框背景
+    UIImage *imageViewContentBackground = [Util adjustImage:[UIImage imageNamed:@"contentbox"]];
+    UIImageView *backgroundImage = [[UIImageView alloc] initWithImage:imageViewContentBackground];
+    backgroundImage.frame = _viewContent.bounds;
+    [_viewContent addSubview:backgroundImage];
+    _viewContent.backgroundColor = [UIColor clearColor];
+    [_viewContent sendSubviewToBack:backgroundImage];
 }
 ;
 - (void)didReceiveMemoryWarning
