34fd50b4351ff8a34c16a98405de24f741ac55c1
imsuker <imsuker@qq.com>
2013-09-02 00:34:57 +0800

    设置badgenumber

--- Joke/AppDelegate.m
+++ Joke/AppDelegate.m
@@ -110,6 +110,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     [[UserModel shareInstance] save];
